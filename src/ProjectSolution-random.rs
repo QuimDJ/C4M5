5111d446cee392cd97171b56ddf1caf97c2c39f8
esbrinachain <ebrinachain@email.com>
2026-01-13 22:57:53 +0100

    c1

--- src/ProjectSolution-random.rs
+++ src/ProjectSolution-random.rs
@@ -1,9 +1,14 @@
+use rand::{seq::SliceRandom, Rng, rng};
+
 #[derive(Debug, Clone, Copy)]
 enum Suit {
     Clubs,
     Spades,
     Hearts,
     Diamonds,
+}
+impl Suit {
+    const ALL:[Suit;4]=[Suit::Spades, Suit::Hearts,Suit::Diamonds,Suit::Clubs];
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -16,13 +21,18 @@ enum Rank {
     Seven,
     Eight,
     Nine,
+    Ten,
     Jack,
     Queen,
     King,
     Ace,
     Joker,
 }
-#[derive(Debug)]
+impl Rank {
+    const ALL:[Rank;13]=[Rank::Two,Rank::Three,Rank::Four,Rank::Five,Rank::Six,Rank::Seven,
+    Rank::Eight,Rank::Nine,Rank::Ten,Rank::Jack,Rank::Queen,Rank::King,Rank::Ace];
+}
+#[derive(Debug, Copy, Clone)]
 struct Card {
     rank:Rank,
     suit:Option<Suit>,
@@ -36,16 +46,71 @@ struct Deck {
 impl Deck{
     fn new(&mut self){
         
-        for itemSuit in Suit.iter(){
-            for itemRank in Rank.iter(){
-                self.cards.push(Card{rank:itemRank, suit:Some(itemSuit)})
+        for itemSuit in Suit::ALL.iter(){
+            for itemRank in Rank::ALL.iter(){
+                self.cards.push(Card{rank:*itemRank, suit:Some(*itemSuit)})
             }
-        } 
+        }
+    }
+    fn baraja_mazo(&mut self) {
+        self.cards.shuffle(&mut rng());
+    }
+    
+    fn insertar_comodines_2(&mut self, joker:Vec<Card>) {
+        
+        let pos1=(0..2).map(|_| rng().random_range(0..52)).collect::<Vec<usize>>();
+        println!("{pos1:?}");
+        self.cards.insert(pos1[0],joker[0]);
+        self.cards.insert(pos1[1], joker[1]);
+    }
+    fn delete_random(&mut self){
+        //if rng().random_ratio(65,100){
+        if rng().random_bool(0.65){
+            let pos_to_delete:usize=rng().random_range(0..52);
+            self.cards.remove(pos_to_delete);
+        }
     }
 }
 
 fn main(){
 
+    // Creamos el mazo
+    let mut d1=Deck{cards:vec![]};
+    // Lo llenamos de cartas
+    d1.new();
+    // Comprobamos que estan las 52 cartas
+    println!("{:?}",d1);
+    // Barajamos el mazo
+    d1.baraja_mazo();
+    println!("{:?}",d1);
+    // insertamos los jokers=> Card{rank:Rank::Joker, suit:None}
+    let jokers=vec![Card{rank:Rank::Joker, suit:None},Card{rank:Rank::Joker, suit:None}];
+    d1.insertar_comodines_2(jokers);
+    println!("{:?}",d1);
+    for _ in 1..=10 {
+        d1.delete_random();
+        println!("{:?}", d1.cards.len());
+    }
 
+}
+
+#[cfg(test)]
+mod test{
+    use super::*;
+    #[test]
+    fn chequeo_mazo_sin_jokers(){
+        let mut d1=Deck{cards:vec![]};
+        d1.new();
+        assert_eq!(d1.cards.len(),52,"El mazo no tiene 52 cartas");
+
+    }
+    #[test]
+    fn chequeo_mazo_con_jokers(){
+        let mut d1=Deck{cards:vec![]};
+        d1.new();
+        assert_eq!(d1.cards.len(),54,"El mazo no tiene 54 cartas");
+
+    }
+    
 
 }
